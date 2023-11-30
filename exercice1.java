f86825bf3a2d4eb42f235704a181a01d1c5f770d
SafaZriba <132261221+SafaZriba@users.noreply.github.com>
2023-11-30 21:48:48 +0100

    Update exercice1.java

--- exercice1.java
+++ exercice1.java
@@ -1,44 +1,157 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.util.ArrayList;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
-class exercice1 {
-	void button() {
-		Frame f = new Frame();
-		Button factoriButton = new Button("factoriel");
-		factoriButton.setBounds(100, 50, 50, 50);
-		f.add(factoriButton);
-		Button moyenneButton = new Button("moyenneButton");
-		moyenneButton.setBounds(100, 101, 50, 50);
-		f.add(moyenneButton);
-		Button cancelButton = new Button("CANCLE");
-		cancelButton.setBounds(100, 150, 80, 50);
-		f.add(cancelButton);
-		f.setSize(500, 500);
-		f.setLayout(null);
-		f.setVisible(true);
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+import javax.swing.undo.UndoManager;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+class exercice1 extends JFrame{
+	JButton button;
+	JTextField nombre;
+	JTextArea resultat;
+	JTextArea resutat_Factoriel;
+    JTextArea resultat_Moyenne;
+	JTextArea inputTextArea_Factoriel;
+	JTextField inputTextArea_Moyenne;
+	UndoManager undoManager;
+
+	exercice1(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new java.awt.FlowLayout());
+
+		JTabbedPane tabbedPane = new JTabbedPane();
+		JPanel factorielPanel = new JPanel();
+		JPanel moyennePanel = new JPanel();
+		nombre = new JTextField(20);
+		inputTextArea_Factoriel = new JTextArea(20, 20);
+		inputTextArea_Moyenne = new JTextField(20);
+		undoManager = new UndoManager(); 
+        Document doc = nombre.getDocument();
+		Document doc1 = inputTextArea_Moyenne.getDocument();
+        doc.addUndoableEditListener(undoManager); 
+		doc1.addUndoableEditListener(undoManager); 
+
+        nombre.addKeyListener(new KeyListener() { 
+            public void keyTyped(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+                if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+                    if (undoManager.canUndo()) {
+                        undoManager.undo();
+                    }
+                }
+            }
+            public void keyReleased(KeyEvent e) {}
+        });
+		inputTextArea_Moyenne.addKeyListener(new KeyListener() { 
+            public void keyTyped(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+                if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+                    if (undoManager.canUndo()) {
+                        undoManager.undo();
+                    }
+                }
+            }
+            public void keyReleased(KeyEvent e) {}
+        });
+		nombre.getDocument().addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+			public void removeUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+			public void changedUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+		});
+
+		nombre.getDocument().addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+			public void removeUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+			public void changedUpdate(DocumentEvent e){
+				updateTextArea_Factoriel();
+			}
+		});
+
+		resultat = new JTextArea(5, 20);
+		resultat.setEditable(false);
+		factorielPanel.add(nombre);
+		factorielPanel.add(new JScrollPane(resultat));
+		tabbedPane.addTab("Factoriel", factorielPanel);
+		
+
+		resultat_Moyenne = new JTextArea(5, 20);
+		resultat_Moyenne.setEditable(false);
+		moyennePanel.add(new JScrollPane(inputTextArea_Moyenne));
+		moyennePanel.add(new JScrollPane(resultat_Moyenne));
+		tabbedPane.addTab("Moyenne", moyennePanel);
+
+
+		this.add(tabbedPane);
+
+		this.pack();
+		this.setVisible(true);
 	}
-	JTextArea styledTextArea = StylePage.createStyledTextArea();
-	frame.add(styledTextArea);
-	JTextArea styledTextArea1 = StylePage.createStyledTextArea();
-	frame.add(styledTextArea1);
-	public int calculateFactorial(int n) throws IllegalArgumentException {
+
+	private void updateTextArea_Factoriel(){
+		try{
+			String inputTextArea = nombre.getText();
+			int n = Integer.parseInt(inputTextArea);
+			int factorial = calculFactoriel(n);
+			resultat.setText("Factorial: "+factorial);
+		}catch(IllegalArgumentException ex){
+			resultat.setText("Invalid input: "+ex.getMessage());
+		}
+	}
+
+	private void updateTextArea_Moyenne() {
+        try {
+            String inputText = inputTextArea_Moyenne.getText();
+            String[] nbr = inputText.split("\\s+");
+            ArrayList<Integer> nbrList = new ArrayList<>();
+            for (String string : nbr) {
+                nbrList.add(Integer.parseInt(string));
+            }
+            double average = calculateAverage(nbrList);
+            resultat_Moyenne.setText("Moyenne: " + average);
+        } catch (IllegalArgumentException ex) {
+            resultat_Moyenne.setText("Invalid input: " + ex.getMessage());
+        }
+    }
+
+	public int calculFactoriel(int n) throws IllegalArgumentException {
         if (n < 0) {
             throw new IllegalArgumentException("Le nombre doit être positif ou nul pour calculer la factorielle.");
         }
         if (n == 0 || n == 1) {
             return 1;
         }
-        return n * calculateFactorial(n - 1);
+        return n * calculFactoriel(n - 1);
     }
 	double calculateAverage(ArrayList<Integer> numbers) {
         if (numbers.isEmpty()) {
-            return 0.0; // Handle the case where the ArrayList is empty to avoid division by zero
+            throw new IllegalArgumentException("ArrayList est vide.");
         }
 		else if (numbers.size() > 4) {
-			throw new Exception("La longueur du tableau est supérieure à 4.");
+			throw new IllegalArgumentException("La longueur du tableau est supérieure à 4.");
 		}
         int sum = 0;
         for (int number : numbers) {
@@ -47,17 +160,11 @@ class exercice1 {
 
         return (double) sum / numbers.size();
     }
-
-	public static void main(String a[]) {
-		exercice1 obj = new exercice1();
-        obj.button();
-
-		int result = obj.calculateFactorial(5);
-        System.out.println("Factorial of 5: " + result);
-		ArrayList<Integer> numberList = new ArrayList<>();
-        numberList.add(10);
-        numberList.add(20);
-        numberList.add(30);
+	
+	public static void main(String[] a) {
+		SwingUtilities.invokeLater(() -> {
+			exercice1 me = new exercice1();
+		});
 	}
 }
 
